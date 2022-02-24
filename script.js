4eaf0ed52dde5be936b0fd63e81de2e36f081f8a
ivanhern3868 <83474382+ivanhern3868@users.noreply.github.com>
2022-02-24 15:49:17 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-
- var principal = document.getElementById("principal").value;
- var rate = document.getElementById("rate").value;
- var years = document.getElementById("years").value;
- var interest = principal * years * rate /100;
- var year = new Date().getFullYear()+parseInt(years);
-
- function updateRate() 
- {
-     var rateval = document.getElementById("rate").value;
-     document.getElementById("rate_val").innerText=rateval;
- }
-
-function compute()
-{
-    p = document.getElementById("principal").value;
+function compute(){
+var principal = document.getElementById("principal").value;
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+var interest = principal * (1 + ((rate / 100) * years));
+let interest2 = (Math.round(interest * 100) / 100).toFixed(2);
+var year = new Date().getFullYear() + parseInt(years);
+document.getElementById("result").innerHTML="If you deposit \<mark\>"+principal+"\</mark>,<br\> at an interest rate of \<mark\>"+rate+"%\</mark>\<br\>You will receive an amount of \<mark>"+interest2+"\</mark>,\<br\> in the year \<mark>"+year+"\</mark>\<br\>";
 }
-        
+
+function validatePrincipal(){
+    if (document.getElementById("principal").value <= 0)
+    {
+    alert("Enter a positive number")
+    document.getElementById("principal").focus();
+    }
+}
